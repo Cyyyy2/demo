dfc804f241208df4eb8c843cae279bbf97246088
黄嘉铭 <huangjiaming@jimitc.com~>
2023-02-28 22:18:05 +0800

    feat:样式

--- src/pages/loginPages.vue
+++ src/pages/loginPages.vue
@@ -1,16 +1,30 @@
 <template>
   <div class="login_panel">
     <input type="text" placeholder="Sign In" class="login_input">
-    <button class="login_btn"></button>
+    <span class="login_sp"><el-icon color="white"><ArrowRightBold /></el-icon></span>
   </div>
 </template>
 <script setup lang="ts">
 
 </script>
-<style lang="less" scoped>
+
+<style lang="scss" scoped>
 .login_panel {
     display: flex;
     justify-content: center;
     align-items: center;
+    background:#9b59b6;
+    padding: 10px;
+    border-radius: 20px;
+    .login_input{
+      outline: none;
+      height: 25px;
+      // background:#9b59b6;
+      // border: 0;
+      color:#fff;
+    }
+    .login_sp {
+
+    }
 }
 </style>
